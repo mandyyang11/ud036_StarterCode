8a5ff263da88dd220cc521c9ad3400d4154efc6f
mandyyang11 <mandyyang11@yahoo.com>
2017-04-16 13:32:27 -0700

    feat: created media class
    created media class to display the movie title, poster url, and the
    trailer url

---
+++ media.py
@@ -0,0 +1,12 @@
+#declare Movie class
+class Movie:
+    #initialize the class
+    def __init__(self, title, poster_image_url, trailer_youtube_url):
+        #assign variables
+        self.title = title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
+        
+        
+
+        
